bde1b0ae00989597c985be7a0418d29afc26e936
Luiserebii <luis@serebii.io>
2019-04-03 22:40:27 -0400

    Finishing migration file

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const Pokethpastes = artifacts.require("Pokethpastes");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(Pokethpastes);
 };
